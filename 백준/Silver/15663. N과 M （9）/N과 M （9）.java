a1cc59306612d9fa4ed2a57139bcf8273107905f
jyh0402 <duddgh0402@gmail.com>
2025-02-17 00:53:28 +0900

    [Silver II] Title: N과 M (9), Time: 112 ms, Memory: 18076 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int M;
+	static int[] p;
+	static int[] nums;
+	static boolean[] visited; 
+	static StringBuilder answer;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		p = new int[N];
+		
+		answer = new StringBuilder();
+		nums = new int[M];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			p[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(p);
+		visited = new boolean[N];
+		perm(0,0);
+		System.out.println(answer);
+	}
+
+	private static void perm(int depth, int start) {
+		int prv = 0;
+		if (depth == M) {
+			for (int i = 0 ; i < M ; i++) {
+				answer.append(nums[i]).append(" ");
+			}
+			answer.append("\n");
+			return ;
+		}
+		for (int i = 0 ; i < N ; i++) {
+			if (visited[i]) continue;
+			if (p[i] == prv) {
+				continue;
+			}
+			prv = p[i];
+			visited[i] = true;
+			nums[depth] = p[i];
+			perm(depth+1,i+1);
+			nums[depth] = 0;
+			visited[i] = false;
+		}
+	}
+
+}
