2e8eac8566ab49b0bb880fe0caa209498db3eefe
jyh0402 <duddgh0402@gmail.com>
2025-02-27 09:18:41 +0900

    [Silver II] Title: 이항 계수 2, Time: 64 ms, Memory: 11540 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N,K;
+	static long an1,an2,an3;
+	static long p = 10007;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		int A = N-K;
+		an1 = 1;
+		an2 = 1;
+		an3 = 1;
+		for (int i = N; i > 0; i--) {
+			an1 *= i;
+			an1 %= p;
+		}
+		
+		for (int i = A; i > 0; i--) {
+			an2 *= i;
+			an2 %= p;
+		}
+		for (int i = K; i > 0; i--) {
+			an3 *= i;
+			an3 %= p;
+		}
+		
+		an2 = (an2 * an3) % p;
+		
+		long ans = (an1 * cal(an2,p-2)) % p;
+		System.out.println(ans);
+	}
+	private static long cal(long x, long l) {
+		long result;
+		if (l == 1) return x;
+		else if (l % 2 == 0) {
+			result = cal(x,l/2) % p;
+			return result * result;
+		} else {
+			result = cal(x,l/2) % p;
+			return result * result *x;
+		}
+	}
+
+}
