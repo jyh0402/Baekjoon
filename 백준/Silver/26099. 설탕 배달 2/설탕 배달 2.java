719f002bef91cbacb6bd066df97974e5c214c0fe
jyh0402 <duddgh0402@gmail.com>
2025-02-27 09:18:11 +0900

    [Silver IV] Title: 설탕 배달 2, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Silver/26099. 설탕 배달 2/설탕 배달 2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+	static long N;
+	static long tot;
+	static long remain;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Long.parseLong(st.nextToken());
+		if (N == 4 || N == 7) {
+			System.out.println(-1);
+			return;
+		}
+		tot = N / 5;
+		remain = N % 5;
+		if (remain == 1 || remain == 3) {
+			tot++;
+		} else if (remain == 2 || remain == 4) {
+			tot += 2;
+		}
+		System.out.println(tot);
+	}
+
+}
