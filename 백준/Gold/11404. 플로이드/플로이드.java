2fd5a415e3051fce3c0d77a53554eb76ca419e57
jyh0402 <duddgh0402@gmail.com>
2025-04-03 18:27:53 +0900

    [Gold IV] Title: 플로이드, Time: 312 ms, Memory: 42572 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static final int INF = Integer.MAX_VALUE/2;
+	static int N,M;
+	static int[][] map;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			Arrays.fill(map[i], INF);
+			map[i][i] = 0;
+		}
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken())-1;
+			int b = Integer.parseInt(st.nextToken())-1;
+			int c = Integer.parseInt(st.nextToken());
+			map[a][b] = Math.min(map[a][b], c);
+		}
+		
+		for (int k = 0; k < N; k++) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (map[i][k] + map[k][j] < map[i][j]) {
+						map[i][j] = map[i][k] + map[k][j];
+					}
+				}
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (map[i][j] == INF) {
+					map[i][j] = 0;
+				}
+				sb.append(map[i][j]+ " ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
