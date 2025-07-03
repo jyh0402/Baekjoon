1ec3f7cafd5b02b88fcc9b966116629c3396a269
jyh0402 <duddgh0402@gmail.com>
2025-07-03 13:34:25 +0900

    [Silver III] Title: 배열 복원하기, Time: 360 ms, Memory: 52620 KB -BaekjoonHub

--- 백준/Silver/16967. 배열 복원하기/배열 복원하기.java
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.java
@@ -14,26 +14,17 @@ public class Main {
 		Y = Integer.parseInt(st.nextToken());
 		int R = H+X;
 		int C = W+Y;
-		int[][] arr = new int[R][C];
 		int[][] A = new int[H][W];
 		for (int i = 0; i < R; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < C; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		for (int i = 0; i < R; i++) {
-			for (int j = 0; j < C; j++) {
-				if (arr[i][j] == 0) continue;
-				if (i < X || j < Y)
-					A[i][j] = arr[i][j];
-				else if (i >= H || j >= W) {
-					if(A[i-X][j-Y] != 0) continue;
-					A[i-X][j-Y] = arr[i][j];
-				} else {
-					if(A[i][j] != 0) continue;
-					A[i][j] = arr[i][j] - A[i-X][j-Y]; 
-				}
+				int v = Integer.parseInt(st.nextToken());
+				if (i < H && j < W) {
+                    if (i >= X && j >= Y) {
+                        v -= A[i - X][j - Y];
+                    }
+                    A[i][j] = v;
+                }
 			}
 		}
 		StringBuilder sb = new StringBuilder();
