3acb8a411fed80ff46de423994390638c6f2775d
jyh0402 <duddgh0402@gmail.com>
2025-08-11 20:23:07 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 64 ms, Memory: 11772 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,68 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int N;
-	static int[][] map;
-	static int cnt;
-	static int[][] visit;
-	static int[] dr = {0,1,1};
-	static int[] dc = {1,1,0};
-	static ArrayDeque<int[]> que;
+	static boolean[][] home;
+	static long[][][] dp;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
-		map = new int[N][N];
-		visit = new int[N][N];
+		home = new boolean[N][N];
+		dp = new long[N][N][3];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+				int n = Integer.parseInt(st.nextToken());
+				if (n == 1) {
+					home[i][j] = true;
+				}				
 			}
 		}
-		que = new ArrayDeque<>();
-		que.offer(new int[] {0,1,0});
-		while(!que.isEmpty()) {
-			int[] cur = que.poll();
-			if (cur[2] == 0) {
-				for (int d = 0; d < 2; d++) {
-					int nr = cur[0] + dr[d];
-					int nc = cur[1] + dc[d];
-					if (!check(nr,nc) || map[nr][nc] == 1) continue;
-					if (d == 1 && (map[nr-1][nc] != 0 || map[nr][nc-1] != 0)) continue;
-					visit[nr][nc]++;
-					que.offer(new int[] {nr,nc,d});
-				}
-			} else if (cur[2] == 1) {
-				for (int d = 0; d < 3; d++) {
-					int nr = cur[0] + dr[d];
-					int nc = cur[1] + dc[d];
-					if (!check(nr,nc) || map[nr][nc] == 1) continue;
-					if (d == 1 && (map[nr-1][nc] != 0 || map[nr][nc-1] != 0)) continue;
-					visit[nr][nc]++;
-					que.offer(new int[] {nr,nc,d});
-				}
-			} else if (cur[2] == 2) {
-				for (int d = 1; d <= 2; d++) {
-					int nr = cur[0] + dr[d];
-					int nc = cur[1] + dc[d];
-					if (!check(nr,nc) || map[nr][nc] == 1) continue;
-					if (d == 1 && (map[nr-1][nc] != 0 || map[nr][nc-1] != 0)) continue;
-					visit[nr][nc]++;
-					que.offer(new int[] {nr,nc,d});
-				}
+		dp[0][1][0] = 1;
+		for (int i = 0; i < N; i++) {
+			for (int j = 2; j < N; j++) {
+				if (home[i][j]) continue;
+				dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][2];
+				
+				if (i == 0) continue;
+				
+				dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2];
+				
+				if (home[i][j-1] || home[i-1][j]) continue;
+				
+				dp[i][j][2] = dp[i-1][j-1][0] + dp[i-1][j-1][1] + dp[i-1][j-1][2];
+				
 			}
 		}
 		
-		
-		System.out.println(visit[N-1][N-1]);
+		System.out.println(dp[N-1][N-1][0]+dp[N-1][N-1][1]+dp[N-1][N-1][2]);
 	}
-	private static boolean check(int nr, int nc) {
-		return nr >= 0 && nc >= 0 && nr < N && nc < N;
-	}
-
 }
